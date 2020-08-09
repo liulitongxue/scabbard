7c3a12b8c3df29087e163413660417cf649e19fa
liuli001001 <liulitongxue@126.com>
2020-08-10 00:26:56 +0800

    :beer: 测试接口

--- src/main/java/org/acottage/scabbard/ScabbardApplication.java
+++ src/main/java/org/acottage/scabbard/ScabbardApplication.java
@@ -10,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ScabbardApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ScabbardApplication.class);
     }
+
 }
