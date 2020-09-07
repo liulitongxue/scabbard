e0238db813ebd2fc911e5131c9d6422836856d45
liuli001001 <liulitongxue@126.com>
2020-09-07 14:32:42 +0800

    :beer: 后续准备用到的依赖包

--- scabbard-admin/src/main/java/org/acottage/scabbard/admin/ScabbardAdminApplication.java
+++ scabbard-admin/src/main/java/org/acottage/scabbard/admin/ScabbardAdminApplication.java
@@ -3,6 +3,7 @@ package org.acottage.scabbard.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @Author liuli
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/08/24 下午 5:27
  * @Version 1.0
  */
-@SpringBootApplication
-@MapperScan("org.acottage.scabbard")
+@SpringBootApplication(scanBasePackages = "org.acottage.scabbard")
+@MapperScan("org.acottage.scabbard.core.dao")
+@EnableAsync
 public class ScabbardAdminApplication {
 
     public static void main(String[] args) {
--- scabbard-api/src/main/java/org/acottage/scabbard/api/ScabbardApiApplication.java
+++ scabbard-api/src/main/java/org/acottage/scabbard/api/ScabbardApiApplication.java
@@ -3,6 +3,7 @@ package org.acottage.scabbard.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @Author liuli
@@ -10,8 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2020/08/24 下午 6:55
  * @Version 1.0
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.acottage.scabbard")
 @MapperScan("org.acottage.scabbard")
+@EnableAsync
 public class ScabbardApiApplication {
 
     public static void main(String[] args) {
