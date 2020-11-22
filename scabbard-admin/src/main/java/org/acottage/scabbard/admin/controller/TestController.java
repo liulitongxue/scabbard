47b627ee2f6c7792ac509fb6a86edec41c42f79e
liuli001001 <liulitongxue@126.com>
2020-11-22 13:29:22 +0800

    :beer: 测试并发

--- scabbard-admin/src/main/java/org/acottage/scabbard/admin/controller/TestController.java
+++ scabbard-admin/src/main/java/org/acottage/scabbard/admin/controller/TestController.java
@@ -2,6 +2,8 @@ package org.acottage.scabbard.admin.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Api(value="测试接口",tags = "接口信息")
 public class TestController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping()
     @ApiOperation(value="测试controller",tags = "测试controller")
     public ResponseEntity test() {
+        long time = System.currentTimeMillis();
+        logger.debug(String.valueOf(time));
         return ResponseEntity.ok("测试"+"\n"+"测试devtools");
     }
 
--- scabbard-admin/src/main/java/org/acottage/scabbard/admin/controller/TestMyBatisController.java
+++ scabbard-admin/src/main/java/org/acottage/scabbard/admin/controller/TestMyBatisController.java
@@ -4,6 +4,8 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.acottage.scabbard.core.entity.TestMyBatis;
 import org.acottage.scabbard.core.service.TestMyBatisService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +25,16 @@ import javax.annotation.Resource;
 @Api(value="测试接口",tags = "接口信息")
 public class TestMyBatisController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestMyBatisController.class);
+
     @Resource
     private TestMyBatisService testMyBatisService;
 
     @PostMapping("/insert")
     @ApiOperation(value="测试myBatis",tags = "测试myBatis")
     public ResponseEntity insertTestMybatis(@RequestBody TestMyBatis testMyBatis) {
+        long time = System.currentTimeMillis();
+        logger.debug(String.valueOf(time));
         return ResponseEntity.ok("插入数据的id为：" + testMyBatisService.insertTestMybatis(testMyBatis));
     }
 
