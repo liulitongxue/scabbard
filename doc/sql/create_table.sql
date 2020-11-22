0b2411a46c55c2aede37f137a613ed568fc99d57
liuli001001 <liulitongxue@126.com>
2020-11-22 17:58:14 +0800

    :beer: mybatisplus的建表语句

--- doc/sql/create_table.sql
+++ doc/sql/create_table.sql
@@ -43,4 +43,17 @@ CREATE TABLE `test_my_batis_generator`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '测试MyBatisGenerator表' ROW_FORMAT = Dynamic;
 
+-- ----------------------------
+-- Table structure for test_my_batis_plus
+-- ----------------------------
+DROP TABLE IF EXISTS `test_my_batis_plus`;
+CREATE TABLE `test_my_batis_plus`  (
+  `id` bigint(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '名称',
+  `is_del` tinyint(1) UNSIGNED NOT NULL COMMENT '思否逻辑删除',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '测试MyBatisPlus表' ROW_FORMAT = Dynamic;
+
 SET FOREIGN_KEY_CHECKS = 1;
