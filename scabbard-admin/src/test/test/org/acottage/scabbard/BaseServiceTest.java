79470fe88a305fce68610842a136f01c98947c1d
liuli001001 <liulitongxue@126.com>
2020-12-22 23:04:45 +0800

    :beer: 单元测试

---
+++ scabbard-admin/src/test/test/org/acottage/scabbard/BaseServiceTest.java
@@ -0,0 +1,17 @@
+package org.acottage.scabbard;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author liuli
+ * @Email liulitongxue@126.com
+ * @Date 2020/12/21 下午 9:37
+ * @Version 1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseServiceTest {
+    protected String host = "http://localhost:11301";
+}
