1d7ecd192fc2a57a8501bd9bc306234c5416c1ce
liam.liu <liam.liu@coudwise.com>
2023-11-01 10:31:00 +0800

    :beer: 跨域修改

--- scabbard-core/src/main/java/org/acottage/scabbard/core/common/config/CorsConfig.java
+++ scabbard-core/src/main/java/org/acottage/scabbard/core/common/config/CorsConfig.java
@@ -1,10 +1,8 @@
 package org.acottage.scabbard.core.common.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @Author liuli
@@ -15,27 +13,18 @@ import org.springframework.web.filter.CorsFilter;
  * 跨域配置
  */
 @Configuration
-public class CorsConfig {
-
-    /**
-     * 注入Bean
-     *
-     * @return CorsFilter
-     */
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration corsConfiguration = new CorsConfiguration();
-        /* 是否允许请求带有验证信息 */
-        corsConfiguration.setAllowCredentials(true);
-        /* 允许访问的客户端域名 */
-        corsConfiguration.addAllowedOrigin("*");
-        /* 允许服务端访问的客户端请求头 */
-        corsConfiguration.addAllowedHeader("*");
-        /* 允许访问的方法名,GET POST等 */
-        corsConfiguration.addAllowedMethod("*");
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return new CorsFilter(urlBasedCorsConfigurationSource);
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                /* 是否允许请求带有验证信息 */
+                .allowCredentials(true)
+                /* 允许访问的客户端域名 */
+                .allowedOrigins("*")
+                /* 允许服务端访问的客户端请求头 */
+                .allowedHeaders("*")
+                /* 允许访问的方法名,GET POST等 */
+                .allowedMethods("*");
     }
 
 }
